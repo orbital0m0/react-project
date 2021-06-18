5f0c333095145b997607d541614754133454a649
jieun kim <orbital0m0@naver.com>
2021-06-18 14:14:09 +0900

    Styles

--- src/Movie.js
+++ src/Movie.js
@@ -9,7 +9,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
             <div>
                 <h3 className="movie_title">{title}</h3>
                 <h5 className="movie_year">{year}</h5>
-                <ul className="genres">
+                <ul className="movie_genres">
                     {genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}
                 </ul>
                 <p className="movie_summary">{summary}</p>
